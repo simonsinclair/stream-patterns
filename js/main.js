4803b0c8dc7446ec910c964ac164f04ded9c5ec4
Simon Sinclair <simon@sinclair>
2015-01-08 12:50:29 +0000

    Refer to master branch

--- js/main.js
+++ js/main.js
@@ -4,12 +4,41 @@
 (function(w, $, undefined) {
   "use strict";
 
-	var Module = {
+	var App = {
+
+    postLoaded: {
+
+    },
 
     init: function() {
-      // ...
+      App.loadContent();
+      App.bindEvts();
+    },
+
+    loadContent: function() {
+      // Load content in to hidden tab panels.
+    },
+
+    bindEvts: function() {
+      $('#js-component-tabs').on('click', 'a', App.handleTabClick);
+    },
+
+    handleTabClick: function(e) {
+      e.preventDefault();
+
+      $('#js-component-tabs li').removeClass('active');
+      $(this)
+        .parent()
+        .addClass('active');
+
+      App.activateTabPanel( $(this).data('panel-name') );
+    },
+
+    activateTabPanel: function(panelName) {
+      $('.panel').hide();
+      $('.panel.panel--' + panelName).show();
     }
   };
 
-	$(Module.init);
+	$(App.init);
 })(this, jQuery);
